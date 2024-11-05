670fed90bac8e810fb6f66d0cce4610db5301a11
Hung Anh <anhlhpl52119@gmail.com>
2024-11-05 11:24:41 +0700

    chore: change to delete method

--- server/routes/foo/[key].delete.ts
+++ server/routes/foo/[key].delete.ts
@@ -2,9 +2,11 @@ export default defineEventHandler(async (event) => {
   const deleteKey = getRouterParam(event, 'key');
   const current: any = await useStorage('myFileSystem').getItem('foo.json')
   delete current[deleteKey];
+
   await useStorage('myFileSystem').setItem('foo.json', current)
-  
+
   // return data
   const data = await useStorage('myFileSystem').getItem('foo.json')
+  
   return data
 })
